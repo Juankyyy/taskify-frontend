0b334bd3ece5376d4daeb5090cf5c9d9718d61e8
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-11 21:24:41 -0500

    feat: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-// 📦 Imports
+import { Route, Routes } from "react-router-dom";
+import { MainLayout } from "./Layouts/MainLayout";
+import { AuthLayout } from "./Layouts/AuthLayout";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 export const App = () => {
   // 🧠 Logic
 
   return (
     <>
-      <h1>App</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
+          }
+        />
+      </Routes>
     </>
   );
 };
