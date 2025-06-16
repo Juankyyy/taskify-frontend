e6f22d46d921c56713c4cc173213536015e8a3b1
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-16 14:37:14 -0500

    feat: height sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import { Logo } from "./Logo.jsx";
+import { Lists } from "./Lists.jsx";
 
 export const Sidebar = () => {
   // 🧠 Logic
 
   return (
-    <nav className="bg-white flex flex-col p-3 rounded-xl max-w-2xs h-dvh">
+    <nav className="bg-white flex flex-col p-3 rounded-xl w-2xs h-[calc(100dvh-20px)]">
       <Logo />
+      <Lists />
     </nav>
   );
 };
