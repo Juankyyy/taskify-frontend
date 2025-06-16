054cf842a75c7517a67102e8b659764d7c5518f4
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-16 18:34:31 -0500

    feat: new folder tooltip

--- src/components/Sidebar/Folders.jsx
+++ src/components/Sidebar/Folders.jsx
@@ -7,8 +7,10 @@ export const Folders = () => {
   return (
     <section className="flex flex-col">
       <div className="flex w-full items-center justify-between mb-4">
-        <h1 className="text-lg font-bold">Folders</h1>
-        <Plus className="w-7 h-7 p-1 bg-sky-400 rounded-full stroke-3 stroke-white cursor-pointer hover:animate-squeeze hover:animate-duration-500" />
+        <h1 className="text-lg font-bold">Carpetas</h1>
+        <div className="lg:tooltip z-30" data-tip="Crear nueva carpeta">
+          <Plus className="w-7 h-7 p-1 bg-sky-400 rounded-full stroke-3 stroke-white cursor-pointer hover:animate-squeeze hover:animate-duration-500" />
+        </div>
       </div>
 
       <Collapse title={"Tareas diarias"}>
