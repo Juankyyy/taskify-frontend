9845cf9d59ab40c14465b18b94dfbeccfc9ca40c
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-16 14:37:33 -0500

    feat: mb logo

--- src/components/Sidebar/Logo.jsx
+++ src/components/Sidebar/Logo.jsx
@@ -4,8 +4,8 @@ export const Logo = () => {
   // 🧠 Logic
 
   return (
-    <div className="flex gap-1 items-center">
-      <img width="20px" height="20px" src="/public/taskify.png" alt="Taskify Logo" />
+    <div className="flex gap-1 items-center mb-5">
+      <img width="20px" height="20px" src="/taskify.png" alt="Taskify Logo" />
       <h1 className="text-2xl font-bold">Taskify</h1>
     </div>
   );
