16ba58d6a69893c89b5e9a9a3b444ec48bf76293
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-26 18:40:30 -0500

    feat: tasks component

---
+++ src/components/Home/Tasks/Task.jsx
@@ -0,0 +1,33 @@
+import { ChevronDown } from "lucide-react";
+import { Trash2 } from "lucide-react";
+
+export const Task = () => {
+  // 🧠 Logic
+
+  return (
+    <>
+      <div className="flex items-center justify-between gap-3 p-3">
+        <div className="flex items-center gap-3 justify-center">
+          <input
+            type="checkbox"
+            defaultChecked
+            className="checkbox checkbox-info"
+          />
+          <h1>Task</h1>
+        </div>
+        <div className="flex gap-4 items-center">
+          <div className="badge badge-soft badge-outline badge-info">
+            <span className="indicator-item status status-info"></span>
+            Low
+          </div>
+          <ChevronDown className="w-icon h-icon" />
+          <Trash2 className="w-icon h-icon cursor-pointer hover:animate-tada  hover:stroke-red-600" />
+        </div>
+      </div>
+
+      <div className="flex justify-center py-2">
+        <div className="w-[95%] border-b border-gray-400 border-dashed"></div>
+      </div>
+    </>
+  );
+};
