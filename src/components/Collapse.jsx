c5f8299d4a573142497aa2d5133928c3b935a41d
Juankyyy <juancamiloherrera960@gmail.com>
2025-07-03 16:53:57 -0500

    Improve folder UI and error handling in sidebar
    Added local state to Collapse component for toggling open/close, improved styling for folder actions, and refined notification logic in useFolders by removing redundant error notifications and updating success messages.

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,11 +1,18 @@
 import { ChevronDown } from "lucide-react";
 import { FolderActions } from "./Sidebar/FolderActions";
+import { useState } from "react";
 
 export const Collapse = ({ title, folderId, children, onDeleteClick }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex items-baseline justify-between">
+    <div className="flex justify-between mb-1 bg-base-100 px-2 rounded-lg">
       <div className="collapse group w-3/4!">
-        <input className="p-0" type="checkbox" />
+        <input onClick={toggle} className="p-0" type="checkbox" />
         <div className="collapse-title flex items-center justify-between p-0">
           <div className="flex items-center">
             <ChevronDown className="w-icon h-icon stroke-3 mr-1 transform duration-300 group-has-[:checked]:rotate-180 " />
@@ -15,7 +22,11 @@ export const Collapse = ({ title, folderId, children, onDeleteClick }) => {
         <div className="collapse-content">{children}</div>
       </div>
 
-      <FolderActions title={title} folderId={folderId} onDeleteClick={onDeleteClick} />
+      <FolderActions
+        title={title}
+        folderId={folderId}
+        onDeleteClick={onDeleteClick}
+      />
     </div>
   );
 };
