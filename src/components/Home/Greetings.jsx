dec4882951db491d2740ff2c92c732f019dd6335
Juankyyy <juancamiloherrera960@gmail.com>
2025-07-09 12:07:20 -0500

    feat: random quotes

--- src/components/Home/Greetings.jsx
+++ src/components/Home/Greetings.jsx
@@ -1,16 +1,19 @@
 import { TimeGreeting } from "../../utils/timeGreeting";
+import { randomQuotes } from "../../utils/randomQuotes";
 
 export const Greetings = () => {
   const username = localStorage.getItem("username");
 
   const { dateName } = TimeGreeting();
 
+  const { quote } = randomQuotes();
+
   return (
     <section className="bg-base-200 p-5 rounded-xl w-full">
       <h1 className="font-bold text-3xl">
         {dateName}, {username ? username : "Unknown User"}!
       </h1>
-      <p className="text-gray-500">¿Qué tienes planeado para hoy?</p>
+      <p className="text-gray-400">{quote}</p>
     </section>
   );
 };
