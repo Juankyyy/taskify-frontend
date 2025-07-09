36872014b7dff58dcbbe6c456cfe8715c3dcc7ce
Juankyyy <juancamiloherrera960@gmail.com>
2025-07-08 23:45:02 -0500

    feat: create task modal ui

--- src/components/Home/Tasks/CreateTaskButton.jsx
+++ src/components/Home/Tasks/CreateTaskButton.jsx
@@ -5,9 +5,14 @@ export const CreateTaskButton = () => {
 
   return (
     <div className="flex justify-center">
-      <div className="group hover:shadow-lg cursor-pointer tranform hover:scale-105 transition-all border-2 hover:border-transparent border-dashed border-blue-300 hover:bg-sky-300 rounded-lg flex gap-2 items-center justify-center w-3/4 h-14 [html[data-theme=dark]_&]:hover:bg-sky-400">
+      <div
+        onClick={() => document.getElementById("create-task-modal").showModal()}
+        className="group hover:shadow-lg cursor-pointer tranform hover:scale-105 transition-all border-2 hover:border-transparent border-dashed border-blue-300 hover:bg-sky-300 rounded-lg flex gap-2 items-center justify-center w-3/4 h-14 [html[data-theme=dark]_&]:hover:bg-sky-400"
+      >
         <Plus className="stroke-2 w-6 h-6 stroke-gray-500 group-hover:stroke-black" />
-        <h1 className="text-gray-500 text-xl group-hover:text-black">Nueva tarea</h1>
+        <h1 className="text-gray-500 text-xl group-hover:text-black">
+          Nueva tarea
+        </h1>
       </div>
     </div>
   );
--- src/components/Home/Tasks/Tasks.jsx
+++ src/components/Home/Tasks/Tasks.jsx
@@ -5,45 +5,51 @@ import { ClipboardList } from "lucide-react";
 import { Pencil } from "lucide-react";
 import { Trash } from "lucide-react";
 import { TaskInfo } from "../../Modals/TaskInfo";
+import { CreateTask } from "../../Modals/CreateTask";
 
 export const Tasks = () => {
   const { selectedList } = useTasks();
 
   return (
-    <section className="bg-base-200 p-5 rounded-xl w-full h-full">
-      <div className="flex justify-between items-center">
-        <div className="breadcrumbs text-sm text-gray-500">
-          <ul>
-            <li className="gap-1">
-              <Folder className="w-icon h-icon stroke-gray-500 cursor-default!" />
-              Carpeta
-            </li>
-            <li className="gap-1 before:opacity-100!">
-              <ClipboardList className="w-icon h-icon stroke-gray-500 cursor-default!" />
-              {selectedList.title}
-            </li>
-          </ul>
-        </div>
-        <div className="flex gap-2 items-center">
-          <button className="btn btn-info p-1 w-8 h-8">
-            <Pencil className="stroke-white" />
-          </button>
-          <button className="btn btn-error p-1 w-8 h-8">
-            <Trash className="stroke-white" />
-          </button>
-        </div>
-      </div>
+    <>
+      {selectedList && (
+        <section className="bg-base-200 p-5 rounded-xl w-full h-full">
+          <div className="flex justify-between items-center">
+            <div className="breadcrumbs text-sm text-gray-500">
+              <ul>
+                <li className="gap-1">
+                  <Folder className="w-icon h-icon stroke-gray-500 cursor-default!" />
+                  Carpeta
+                </li>
+                <li className="gap-1 before:opacity-100!">
+                  <ClipboardList className="w-icon h-icon stroke-gray-500 cursor-default!" />
+                  {selectedList.title}
+                </li>
+              </ul>
+            </div>
+            <div className="flex gap-2 items-center">
+              <button className="btn btn-info p-1 w-8 h-8">
+                <Pencil className="stroke-white" />
+              </button>
+              <button className="btn btn-error p-1 w-8 h-8">
+                <Trash className="stroke-white" />
+              </button>
+            </div>
+          </div>
 
-      <div className="flex flex-col mb-8">
-        <h1 className="font-bold text-3xl">Tareas</h1>
-        <p className="text-gray-400">{selectedList.description}</p>
-      </div>
+          <div className="flex flex-col mb-8">
+            <h1 className="font-bold text-3xl">Tareas</h1>
+            <p className="text-gray-400">{selectedList.description}</p>
+          </div>
 
-      <div>
-        <Task />
-      </div>
+          <div>
+            <Task />
+          </div>
 
-      <TaskInfo />
-    </section>
+          <TaskInfo />
+          <CreateTask />
+        </section>
+      )}
+    </>
   );
 };
